dea5af99c4f34732b738a67f2284372d1eab8cd2
tuxshido <c.metman@e.email>
2022-05-01 06:08:57 +0900

    feat: an input to search post with a title word or a tag

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ db = SQLAlchemy(app)
 class Blogpost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
-    subtitle = db.Column(db.String(50))
+    tag = db.Column(db.String(50))
     author = db.Column(db.String(20))
     date_posted = db.Column(db.DateTime)
     content = db.Column(db.Text)
@@ -122,18 +122,33 @@ def add():
         return render_template('add.html', login=True, name=session['name'])
 
 
+@app.route('/search', methods=['POST'])
+def search():
+    searchword = request.form['searchword']
+    searchby = request.form['searchby']
+    if searchby == "title":
+        posts = Blogpost.query.filter(Blogpost.title.contains(
+            searchword)).order_by(Blogpost.date_posted.desc()).all()
+    else:
+        posts = Blogpost.query.filter(Blogpost.tag.contains(
+            searchword)).order_by(Blogpost.date_posted.desc()).all()
+    if "google_id" not in session:
+        return render_template('index.html', posts=posts, login=False, name=None)
+    else:
+        return render_template('index.html', posts=posts, login=True, name=session['name'])
+
+
 @app.route('/addpost', methods=['POST'])
 def addpost():
     title = request.form['title']
-    subtitle = request.form['subtitle']
+    tag = request.form['tag']
     author = session['name']
     content = request.form['content']
-
-    post = Blogpost(title=title, subtitle=subtitle, author=author,
-                    content=content, date_posted=datetime.now())
-
-    db.session.add(post)
-    db.session.commit()
+    if (title != "" and tag != "" and content != ""):
+        post = Blogpost(title=title, tag=tag, author=author,
+                        content=content, date_posted=datetime.now())
+        db.session.add(post)
+        db.session.commit()
 
     return redirect(url_for('index'))
 
